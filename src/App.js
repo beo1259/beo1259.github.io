91ecfe591a29bc729553f86600eb1ee56f0a1dae
beo1259 <oneilb123@gmail.com>
2023-12-02 18:12:17 -0500

    Final Touches

--- src/App.js
+++ src/App.js
@@ -226,7 +226,7 @@ function App() {
   return (
 
     <>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
       <Header currentSection={currentSection} setCurrentSection={setCurrentSection} />
 
