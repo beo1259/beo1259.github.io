e9c4b9da2bf09338cccf8f1b94ad3371f7d6b152
beo1259 <oneilb123@gmail.com>
2023-12-21 18:31:37 -0500

    added progress bar

--- src/App.js
+++ src/App.js
@@ -60,14 +60,11 @@ function Header({ currentSection, setCurrentSection }) {
 const [scrollTop, setScrollTop] = useState(0);
 
 const onScroll = () => {
-  // This will calculate how many pixels the page is vertically
   const winScroll = document.documentElement.scrollTop;
-  // This is responsible for subtracticing the total height of the page - where the users page is scrolled to
   const height =
     document.documentElement.scrollHeight -
     document.documentElement.clientHeight;
 
-  // This will calculate the final total of the percentage of how much the user has scrolled.
   const scrolled = (winScroll / height) * 100;
 
   setScrollTop(scrolled);
